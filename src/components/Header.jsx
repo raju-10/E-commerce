75d9677e289a3a8a4397188216e7e6c76e561205
Abdessamad7687 <abdessamadahmali@gmail.com>
2023-07-30 14:49:02 +0100

    header-section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,28 @@
 import React from 'react'
 
 const Header = () => {
-  return (
-    <div className="w-full h-20 bg-white border-b-[1px] border-b-gray-800">
-        <div className="max-w-screen-xl h-full mx-auto flex items-center justify-between">
-            <div>
-                <img className="w-28" src="" alt="Logo here" srcset="" />
+    return (
+        <div className="w-full h-20 bg-white border-b-[1px] border-b-gray-800">
+            <div className="max-w-screen-xl h-full mx-auto flex items-center justify-between">
+                <div>
+                    <img className="w-28" src="" alt="Logo here" srcset="" />
+                </div>
+
+                <div>
+                    <ul className="flex items-center gap-8">
+                        <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 cursor-pointer duration-300">Home</li>
+                        <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 cursor-pointer duration-300">Pages</li>
+                        <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 cursor-pointer duration-300">Shop</li>
+                        <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 cursor-pointer duration-300">Elements</li>
+                        <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 cursor-pointer duration-300">Blog</li>
+                    </ul>
+                </div>
             </div>
-        </div>
 
 
-        <div>
-            <ul>
-                <li>Home</li>
-                <li>Pages</li>
-                <li>Shop</li>
-                <li>Elements</li>
-                <li>Blog</li>
-            </ul>
+
         </div>
-    </div>
-  )
+    )
 }
 
 export default Header
