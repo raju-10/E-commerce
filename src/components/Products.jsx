dc79bcf421c2d3b26d853978eea07ee5a1d37023
Abdessamad7687 <abdessamadahmali@gmail.com>
2023-07-30 23:57:06 +0100

    Products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Products = () => {
   return (
-    <div className="flex flex-col items-center gap-4">
-        <h2>Shooping every day! </h2>
+    <div className="flex flex-col items-center gap-4 m-3">
+        <h2 className="text-2xl text-white py-2 bg-black w-80 text-center">Shoping every day! </h2>
+        <span className="w-20 h-1 bg-black"></span>
+        <p className="max-w-[700px] text-gray-600 text-center">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor fuga quisquam odit, qui minus fugiat id beatae accusantium vel animi, porro nostrum dicta vitae voluptas iure adipisci dolore voluptatum natus!
+        </p>
     </div>
   )
 }
