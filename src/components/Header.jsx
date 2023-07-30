76e2bd2b886c2eda9c6d67217ad956673a1c6d0d
Abdessamad7687 <abdessamadahmali@gmail.com>
2023-07-30 15:08:55 +0100

    header-secttion adding fonts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
+import { logo } from '../assets'
 
 const Header = () => {
     return (
         <div className="w-full h-20 bg-white border-b-[1px] border-b-gray-800">
             <div className="max-w-screen-xl h-full mx-auto flex items-center justify-between">
                 <div>
-                    <img className="w-28" src="" alt="Logo here" srcset="" />
+                    <img className="w-28" src={logo} alt="Logo here" srcset="" />
                 </div>
 
                 <div>
                     <ul className="flex items-center gap-8">
-                        <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 cursor-pointer duration-300">Home</li>
-                        <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 cursor-pointer duration-300">Pages</li>
-                        <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 cursor-pointer duration-300">Shop</li>
-                        <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 cursor-pointer duration-300">Elements</li>
-                        <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 cursor-pointer duration-300">Blog</li>
+                        <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 cursor-pointer duration-300 font-titleFont">Home</li>
+                        <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 cursor-pointer duration-300 font-titleFont ">Pages</li>
+                        <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 cursor-pointer duration-300 font-titleFont ">Shop</li>
+                        <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 cursor-pointer duration-300 font-titleFont ">Elements</li>
+                        <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 cursor-pointer duration-300 font-titleFont ">Blog</li>
                     </ul>
                 </div>
             </div>
